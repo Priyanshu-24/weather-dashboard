f0156a7fb5fb24a316c09153766e936ddaefe994
priyanshu <priyanshu24pandey@gmail.com>
2024-06-22 15:20:58 +0530

    added basic components skeletal

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import Search from "./components/Search";
+import WeatherDisplay from "./components/WeatherDisplay";
+import styled from "styled-components";
+
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+`;
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <MainContainer>
+      <h1>Weather Dashboard</h1>
+      <Search />
+      <WeatherDisplay />
+    </MainContainer>
+  );
 };
 
 export default App;
